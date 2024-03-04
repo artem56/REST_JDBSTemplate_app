dd4004583226de59fd8f622bb80b73a8ae54aed7
OneDudeStudio <alexyashtingin@mail.ru>
2024-03-05 01:34:31 +0400

    1

---
+++ src/main/java/com/example/application_dev/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.application_dev.controller;
+
+public class UserController {
+}
---
+++ src/test/java/com/example/application_dev/ApplicationDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.application_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
