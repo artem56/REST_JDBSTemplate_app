0bd4a3acbe5065ea6c8979827d2a23b0653b1487
OneDudeStudio <alexyashtingin@mail.ru>
2024-03-05 02:26:23 +0400

    2

---
+++ src/main/java/com/example/application_dev/Entity/UserEntity.java
@@ -0,0 +1,4 @@
+package com.example.application_dev.Entity;
+
+public class UserEntity {
+}
--- src/main/java/com/example/application_dev/controller/UserController.java
+++ src/main/java/com/example/application_dev/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.example.application_dev.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
-}
+    @GetMapping
+    public ResponseEntity getUser()
+    {
+       try
+       {
+        return ResponseEntity.ok("Сервер работает");
+       }
+       catch(Exception e)
+       {
+        return ResponseEntity.badRequest().body("Произошла ошибка");
+       }
+    }
+    }
