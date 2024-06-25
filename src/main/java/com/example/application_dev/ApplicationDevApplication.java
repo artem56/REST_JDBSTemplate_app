8ffd6523887be613a008d2f27fb18b164399020e
artem56 <artemyashtingin@gmail.com>
2024-06-25 23:37:36 +0400

    comment drop table

--- src/main/java/com/example/application_dev/ApplicationDevApplication.java
+++ src/main/java/com/example/application_dev/ApplicationDevApplication.java
@@ -21,11 +21,11 @@ public class ApplicationDevApplication implements CommandLineRunner {
 	@Override
 	public void run(String... strings) throws Exception {
 
-		log.info("Creating tables");
-		jdbcTemplate.execute("Drop table if exists users ");
-		log.info("Drop table users");
-		jdbcTemplate.execute("Create table if not EXISTS users (user_id serial PRIMARY KEY, first_name varchar(35), last_name varchar (35)) ");
-		log.info("Create table users");
+//		log.info("Creating tables");
+//		jdbcTemplate.execute("Drop table if exists users ");
+//		log.info("Drop table users");
+//		jdbcTemplate.execute("Create table if not EXISTS users (user_id serial PRIMARY KEY, first_name varchar(35), last_name varchar (35)) ");
+//		log.info("Create table users");
 
 
 	}
